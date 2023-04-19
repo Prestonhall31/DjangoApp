67a8f578d3cb71a815798788c6fd96f6c9ece8eb
Preston <prestonhall31@gmail.com>
2023-04-18 18:56:05 -0700

    Added base page for template inheritance.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'Mary',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020'
+    }
+]
+
 def home(request):
-    return render(request, template_name='blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, template_name='blog/home.html', context=context)
 
 
 def about(request):
-    return render(request, template_name='blog/about.html')
+    return render(request, template_name='blog/about.html', context={'title': 'About'})
+
