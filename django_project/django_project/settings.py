d911129508f1cad326b7cdf5a24d3357e5fb8f9c
Preston <prestonhall31@gmail.com>
2023-04-20 15:30:35 -0700

    Added Users App created View and url

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig', # Make sure to add this everytime you create a new app for the project. 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
