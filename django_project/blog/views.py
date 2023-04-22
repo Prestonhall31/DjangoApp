8b0eca9f79ceadad70f39029867a0e61c79a8f9c
Preston <prestonhall31@gmail.com>
2023-04-21 19:53:55 -0700

    Added create blog feature

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2020'
-    },
-    {
-        'author': 'Mary',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2020'
-    }
-]
+# Code was refactored below
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, template_name='blog/home.html', context=context)
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, template_name='blog/home.html', context=context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<modle>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user # Setting the instance to the current logged in user.
+        return super().form_valid(form)
 
 
 def about(request):
     return render(request, template_name='blog/about.html', context={'title': 'About'})
+
 
